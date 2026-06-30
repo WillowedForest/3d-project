d9a0147e2665fbbd434066c9ff78d604a4ebbb65
willow <joe.phillips935@proton.me>
2026-06-30 12:31:41 +0100

    take 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 
 #include "raylib.h"
-#include<bits/stdc++.h>
 
 const Vector2 screen{800.0f, 480.0f};
 char outPut[5];
@@ -12,7 +11,7 @@ int main() {
 
     Camera3D camera;
     camera.position = {10.0f, 10.0f, 10.0f};
-    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.target = Vector3{2.0f, 2.0f, 2.0f};
     camera.up = (Vector3{0.0f, 1.0f, 0.0f});
     camera.fovy = 90.0;
     camera.projection = CAMERA_PERSPECTIVE;
